0d3b227bf4c3c0aa5ffc52082774a9ea1d7b2e82
WhaleKimm <tmdals128551@gmail.com>
2024-07-03 14:18:24 +0900

    라우터 추가

--- src/main.js
+++ src/main.js
@@ -1,4 +1,12 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router'; // 라우터 가져오기
 
-createApp(App).mount('#app')
+const app = createApp(App);
+
+if (module.hot) {
+  module.hot.accept();
+}
+
+app.use(router); // 라우터 사용
+app.mount('#app');
